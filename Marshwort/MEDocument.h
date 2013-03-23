7a7da8aba26cff68e7b2eebf3373980b622ab1cb
Andrey M <meamka@ya.ru>
2013-03-23 23:42:56 +0400

    Initial Commit

---
+++ Marshwort/MEDocument.h
@@ -0,0 +1,13 @@
+//
+//  MEDocument.h
+//  Marshwort
+//
+//  Created by Andrey M. on 23.03.13.
+//  Copyright (c) 2013 Andrey M. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MEDocument : NSDocument
+
+@end
