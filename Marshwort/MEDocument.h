b9417efadafd50f923c0f77f75723a693eafdab4
Andrey M <meamka@ya.ru>
2013-03-25 10:49:58 +0400

    Document-based version of marshwort

--- Marshwort/MEDocument.h
+++ Marshwort/MEDocument.h
@@ -8,6 +8,27 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface MEDocument : NSDocument
+@interface MEDocument : NSDocument <NSTextViewDelegate> {
+    NSArray *languagesFrom;
+    NSArray *languagesTo;
+    NSString *sourceText;
+    NSString *translatedText;
+    NSTimer *translateTimer;
+    NSUserDefaults *userDefaults;
+}
 
+@property (unsafe_unretained) IBOutlet NSTextView *sourceTextView;
+@property (unsafe_unretained) IBOutlet NSTextView *translatedTextView;
+@property (weak) IBOutlet NSPopUpButtonCell *languageFromBox;
+@property (weak) IBOutlet NSPopUpButtonCell *languageToBox;
+@property (strong) IBOutlet NSArrayController *langsFromArrayController;
+@property (strong) IBOutlet NSArrayController *langsToArrayController;
+@property (weak) IBOutlet NSProgressIndicator *inworkIndicator;
+
+- (IBAction)reverseLanguages:(id)sender;
+
+- (IBAction)changeLanguage:(id)sender;
+- (void)translate:(NSString *)text fromLanguage:(NSString *)from toLanguage:(NSString *)to withFormat:(NSString *)format;
+- (IBAction)beginTranslate:(id)sender;
+- (void)beginTranslateTimer;
 @end
